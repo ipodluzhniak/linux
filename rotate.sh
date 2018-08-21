9201e1e88a15a9eee9a4591414b9afb2f3f2134b
ipodluzhniak <ipodluzhniak@gmail.com>
2018-08-21 21:54:12 +0200

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 xrandr --output LVDS1 --rotate left
+#xinput list
+
+#normal)
+#xinput set-prop "$TOUCHPAD"    "$TRANSFORM" 1 0 0 0 1 0 0 0 1
+#inverted)
+#xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" -1 0 1 0 -1 1 0 0 1
+#left)
+#xinput set-prop "$TOUCHPAD"    "$TRANSFORM" 0 -1 1 1 0 0 0 0 1
+#right)
+#xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" 0 1 0 -1 0 1 0 0 1
